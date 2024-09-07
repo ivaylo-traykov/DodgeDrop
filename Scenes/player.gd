bf896d943ce1b4e6e333f0da19edf70455feb0f5
Ivaylo Traykov <ivaylotraykov027@gmail.com>
2024-09-08 00:33:00 +0300

    Imporve player movement, add wrap around the playfield

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,10 +7,16 @@ func _process(delta):
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
+	# Basic left and right movement
 	if direction:
-		velocity.x = direction * speed
-		
+		velocity.x = move_toward(velocity.x, speed * direction, drift)
 	else:
 		velocity.x = move_toward(velocity.x, 0, drift)
+
+	# Warp player from the side of the screen
+	if position.x < 0:
+		position.x = 720
+	elif position.x > 720:
+		position.x = 0
 
 	move_and_slide()
